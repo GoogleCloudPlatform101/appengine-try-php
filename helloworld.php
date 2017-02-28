5c243c473d3043f1788e9b1dcc157f29b07b3efe
GoogleCloudPlatform101 <ngx874@mocs.utc.edu>
2017-02-28 18:36:59 -0500

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -13,7 +13,7 @@
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/mdb.min.css" rel="stylesheet">
         <link href="css/custom.css" rel="stylesheet">
-        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400" rel="stylesheet">
 
     </head>
